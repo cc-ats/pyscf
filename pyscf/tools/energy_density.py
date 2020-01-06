74d0d0ac3d8cd9a1494b8522f44b970f366ae724
yangjunjie <yangjunjie@iMac.local>
2020-01-06 12:35:12 -0600

    test

--- pyscf/tools/energy_density.py
+++ pyscf/tools/energy_density.py
@@ -240,63 +240,31 @@ if __name__ == '__main__':
     print("E elec = %f, ref E elec = %f"
     %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
 
-    rhot = calc_rho_t(mf, coords, dm, ao_value=ao_value)
-    print("E t = %f, E ref = %f"
-    %(
-        lib.einsum("i,i->", weights, rhot),
-        lib.einsum('ij,ji->', mol.intor('cint1e_kin_sph'), dm[0]+dm[1])
-    ))
+    mf = scf.UKS(mol)
+    mf.xc = 'BLYP'
+    mf.verbose = 0
+    mf.kernel()
+    dm = mf.make_rdm1()
 
-    rhov, rhoj = calc_rhov_rhoj(mf, coords, dm, ao_value=ao_value)
-    print("E v = %f, E ref = %f"
-    %(
-        lib.einsum("i,i,i->", weights, rhov, rho),
-        lib.einsum('ij,ji->', mol.intor('cint1e_nuc_sph'), dm[0]+dm[1])
-    ))
-
-    print("E j = %f, E ref = %f"
-    %(
-        lib.einsum("i,i,i->", weights, rhoj, rho),
-        +lib.einsum('ij,ji->', mf.get_j(mol=mol, dm=(dm[0]+dm[1])), dm[0]+dm[1])/2
-    ))
-
-    rhok = calc_rhok(mf, coords, dm, ao_value=ao_value)
-    print("E k = %f, E ref = %f"
-    %(
-        lib.einsum("i,i->", weights, rhok),
-        -lib.einsum('kij,kji->', mf.get_k(mol=mol, dm=dm), dm)/2
-    ))
-
+    rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
+    print('BLYP')
     print("E elec = %f, ref E elec = %f"
-    %(lib.einsum("i,i->", weights, 
-    (rhov + rhoj)*rho + rhot + rhok
-    ), mf.energy_elec()[0]))
+    %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
 
-    # mf = scf.UKS(mol)
-    # mf.xc = 'BLYP'
-    # mf.verbose = 0
-    # mf.kernel()
-    # dm = mf.make_rdm1()
+    mf.xc = 'b3lyp'
+    mf.kernel()
+    dm = mf.make_rdm1()
 
-    # rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
-    # print('BLYP')
-    # print("E elec = %f, ref E elec = %f"
-    # %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
+    rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
+    print('b3lyp')
+    print("E elec = %f, ref E elec = %f"
+    %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
 
-    # mf.xc = 'b3lyp'
-    # mf.kernel()
-    # dm = mf.make_rdm1()
+    mf.xc = 'camb3lyp'
+    mf.kernel()
+    dm = mf.make_rdm1()
 
-    # rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
-    # print('b3lyp')
-    # print("E elec = %f, ref E elec = %f"
-    # %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
-
-    # mf.xc = 'camb3lyp'
-    # mf.kernel()
-    # dm = mf.make_rdm1()
-
-    # rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
-    # print('cam-b3lyp')
-    # print("E elec = %f, ref E elec = %f"
-    # %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
+    rhoe = calc_rho_ene(mf, coords, dm, ao_value=ao_value)
+    print('cam-b3lyp')
+    print("E elec = %f, ref E elec = %f"
+    %(lib.einsum("i,i->", weights, rhoe), mf.energy_elec()[0]))
