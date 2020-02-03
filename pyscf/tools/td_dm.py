8dacdbcb8500da804ffd311690cf883f78a070f5
yangjunjie <yangjunjie@iMac.local>
2020-02-02 22:22:39 -0600

    test

--- pyscf/tools/td_dm.py
+++ pyscf/tools/td_dm.py
@@ -276,7 +276,7 @@ def eval_rt_dm(tdscf, dm_ao, am, e, t_array):
         dm_list[:, :nocc,nocc:] = dm_mo_ov
         dm_list[:, nocc:,:nocc] = dm_mo_vo
         # print(dm_list[0])
-        [print(dm) for dm in dm_list]
+        [print(reduce(numpy.dot, (x, dm, x.T))) for dm in dm_list]
         return dm_list # [reduce(numpy.dot, (x, dm, x.T)) for dm in dm_list]
 
 
