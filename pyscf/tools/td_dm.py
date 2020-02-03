94af22aa7c9c217869139f9470fcaa25b21ce084
yangjunjie <yangjunjie@iMac.local>
2020-02-02 22:13:55 -0600

    test

--- pyscf/tools/td_dm.py
+++ pyscf/tools/td_dm.py
@@ -332,6 +332,7 @@ if __name__ == "__main__":
     am = proj_ex_states(td, dm2)
     print("am = \n", am)
     dms_rks = eval_rt_dm(td, dm2, am, td.e, t_array)
+    print(dm2)
     print(dms_rks[0])
 
     print("*******UKS*******")
