7999f3ebae706839380af2c7ce525db6f5602ee7
yangjunjie <yangjunjie@MacBook-Pro.local>
2020-05-29 22:24:38 -0500

    real time

--- pyscf/rt/__init__.py
+++ pyscf/rt/__init__.py
@@ -1,24 +1,40 @@
-# TODO: refactor the code before adding to FEATURES list by PySCF-1.5 release
-# Copyright 2014-2018 The PySCF Developers. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 1. code style
-#   * Remove the unused modules: numpy, scipy, gto, dft, ...
-#
+# Author: Junjie Yang <yangjunjie0320@gmail.com> Zheng Pei and Yihan Shao
 
-# We should get the lib import working for now let's just do a quick TDSCF.
-#from pyscf.tdscf import bo
-import numpy as np
-import scipy
-import scipy.linalg
-from pyscf import gto, dft, scf, ao2mo
+import pyscf
+
+from pyscf import rt
+from pyscf import scf
+
+from pyscf.rt import util
+from pyscf.rt import chkfile
+
+from pyscf.rt import rhf
+from pyscf.rt import uhf
+from pyscf.rt import rks
+from pyscf.rt import uks
+
+def TDHF(mf):
+    if getattr(mf, 'xc', None):
+        raise RuntimeError('TDHF does not support DFT object %s' % mf)
+    if isinstance(mf, scf.uhf.UHF):
+        mf = scf.addons.convert_to_uhf(mf)
+        return uhf.TDHF(mf)
+    else:
+        mf = scf.addons.convert_to_rhf(mf)
+        return rhf.TDHF(mf)
+
+def TDDFT(mf):
+    if isinstance(mf, scf.uhf.UHF):
+        mf = scf.addons.convert_to_uhf(mf)
+        if getattr(mf, 'xc', None):
+            return uks.TDDFT(mf)
+        else:
+            return uhf.TDHF(mf)
+    else:
+        mf = scf.addons.convert_to_rhf(mf)
+        if getattr(mf, 'xc', None):
+            return rks.TDDFT(mf)
+        else:
+            return rhf.TDHF(mf)
+
+TDSCF = TDDFT
---
+++ pyscf/rt/chkfile.py
@@ -0,0 +1,24 @@
+# Author: Junjie Yang <yangjunjie0320@gmail.com> Zheng Pei and Yihan Shao
+
+import h5py
+from pyscf.lib.chkfile import load_chkfile_key, load
+from pyscf.lib.chkfile import dump_chkfile_key, dump, save
+from pyscf.lib.chkfile import load_mol, save_mol
+
+def load_rt(chkfile):
+    return load_mol(chkfile), load(chkfile, 'rt')
+
+def dump_rt(mol, chkfile, ntime, netot, ndm_ao, overwrite_mol=True):
+    '''save temporary results'''
+    if h5py.is_hdf5(chkfile) and not overwrite_mol:
+        with h5py.File(chkfile, 'a') as fh5:
+            if 'mol' not in fh5:
+                fh5['mol'] = mol.dumps()
+    else:
+        save_mol(mol, chkfile)
+
+    rt_dic  = { "ntime"      :ntime,
+                "ndm_ao"     :ndm_ao,   
+                "netot"      :netot}
+    save(chkfile, 'rt', rt_dic)
+
