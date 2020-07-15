0a99a444cfc0603356daf6ce150b487dd3b13883
Junjie, Yang <244140486@qq.com>
2020-07-15 18:39:26 -0500

    Update test_all.sh

--- pyscf/rt/test/test_all.sh
+++ pyscf/rt/test/test_all.sh
@@ -1,6 +1,6 @@
 rm ./*pdf;
 rm ./*log;
 rm ./*chk;
-# python test_prop_restricted.py   > test_prop_restricted.log;
-# python test_prop_unrestricted.py > test_prop_unrestricted.log; 
-# python test_spectrum.py          > test_spectrum.log; 
+python test_prop_restricted.py   > test_prop_restricted.log;
+python test_prop_unrestricted.py > test_prop_unrestricted.log; 
+python test_spectrum.py          > test_spectrum.log; 
