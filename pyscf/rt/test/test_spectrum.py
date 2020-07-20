1cd1441a71c705c874936409d002e6425aaaa4c2
yangjunjie <yangjunjie@MacBook-Pro.local>
2020-07-20 16:45:07 -0500

    test

--- pyscf/rt/test/test_spectrum.py
+++ pyscf/rt/test/test_spectrum.py
@@ -14,82 +14,58 @@ from pyscf.rt.util   import build_absorption_spectrum
 from pyscf.rt.field  import ClassicalElectricField, constant_field_vec, gaussian_field_vec
 from pyscf.rt.result import read_index_list, read_step_dict, read_keyword_value
 
-adenine =   gto.Mole( atom='''
-  N     -2.2027     -0.0935     -0.0068
-  C     -0.9319      0.4989     -0.0046
-  C      0.0032     -0.5640     -0.0064
-  N     -0.7036     -1.7813     -0.0164
-  C     -2.0115     -1.4937     -0.0151
-  C      1.3811     -0.2282     -0.0017
-  N      1.7207      1.0926     -0.0252
-  C      0.7470      2.0612     -0.0194
-  N     -0.5782      1.8297     -0.0072
-  H      1.0891      3.1044     -0.0278
-  N      2.4103     -1.1617      0.1252
-  H     -3.0709      0.3774     -0.0035
-  H     -2.8131     -2.2379     -0.0191
-  H      2.1765     -2.0715     -0.1952
-  H      3.3110     -0.8521     -0.1580
+water =   gto.Mole( atom='''
+  O     0.00000000    -0.00001441    -0.34824012
+  H    -0.00000000     0.76001092    -0.93285191
+  H     0.00000000    -0.75999650    -0.93290797
   '''
   , basis='6-311+g(d)', symmetry=False).build()
 
-adenine_rks    = scf.RKS(adenine)
-adenine_rks.verbose  = 0
-adenine_rks.xc       = "pbe0"
-adenine_rks.conv_tol = 1e-12
-adenine_rks.kernel()
-dm_init = adenine_rks.make_rdm1()
+water_rks    = scf.RKS(water)
+water_rks.verbose  = 0
+water_rks.xc       = "pbe0"
+water_rks.conv_tol = 1e-12
+water_rks.kernel()
+dm_init = water_rks.make_rdm1()
 
-rttd = rt.TDSCF(adenine_rks)
+rttd = rt.TDSCF(water_rks)
 rttd.total_step     = 8000
 rttd.step_size      = 0.2
 rttd.verbose        = 4
 rttd.dm_ao_init     = dm_init
 rttd.prop_method    = "mmut"
-
-lrtd = tddft.TDDFT(adenine_rks)
-lrtd.verbose = 4
-lrtd.nstates = 30
-lrtd.kernel()
-
-fig, ax = plt.subplots(figsize=(10,6))
-ax.stem(27.2116*lrtd.e, lrtd.oscillator_strength(), linefmt='grey', markerfmt=None, basefmt=" ", use_line_collection=True, label="LR-TDDFT")
-
-field_strength = 1e-4
-gaussian_vec_x   = lambda t: gaussian_field_vec(t, 0.0, 0.02, 0.0, [field_strength, 0.0, 0.0])
-gaussian_field_x = ClassicalElectricField(adenine, field_func=gaussian_vec_x, stop_time=0.5)
-
-gaussian_vec_y   = lambda t: gaussian_field_vec(t, 0.0, 0.02, 0.0, [0.0, field_strength, 0.0])
-gaussian_field_y = ClassicalElectricField(adenine, field_func=gaussian_vec_y, stop_time=0.5)
-
-gaussian_vec_z   = lambda t: gaussian_field_vec(t, 0.0, 0.02, 0.0, [0.0, 0.0, field_strength])
-gaussian_field_z = ClassicalElectricField(adenine, field_func=gaussian_vec_z, stop_time=0.5)
-
 rttd.save_in_disk   = False
 rttd.chk_file       = None
 rttd.save_in_memory = True
 
-rttd.electric_field = gaussian_field_x
-rttd.kernel()
-time = read_keyword_value("t",      result_obj=rttd.result_obj)
-dip1 = read_keyword_value("dipole", result_obj=rttd.result_obj)
-dxx  = dip1[:,0] - dip1[0,0]
+field_strength = 1e-4
+dipole_list = []
+for i in range(3):
+    temp_vec = [0.0, 0.0, 0.0]
+    temp_vec[i] = field_strength
+    temp_delta_vec   = lambda t: gaussian_field_vec(t, 0.0, 0.02, 0.0, temp_vec)
+    temp_delta_field = ClassicalElectricField(water, field_func=temp_delta_vec, stop_time=0.5)
 
-rttd.electric_field = gaussian_field_y
-rttd.kernel()
-dip2 = read_keyword_value("dipole", result_obj=rttd.result_obj)
-dyy  = dip2[:,1] - dip2[0,1]
+    rttd.electric_field = temp_delta_field
+    rttd.kernel()
+    
+    time     = read_keyword_value("t",      result_obj=rttd.result_obj)
+    temp_dip = read_keyword_value("dipole", result_obj=rttd.result_obj)
+    dipole_list.append(temp_dip[:,i] - temp_dip[0,i])
 
-rttd.electric_field = gaussian_field_z
-rttd.kernel()
-dip3 = read_keyword_value("dipole", result_obj=rttd.result_obj)
-dzz  = dip3[:,2] - dip3[0,2]
 
-mw, sigma = build_absorption_spectrum(0.2, time, numpy.array([dxx,dyy,dzz]).T, damp_expo=50.0)
+mw, sigma = build_absorption_spectrum(0.2, time, numpy.array(dipole_list).T, damp_expo=50.0)
+
+fig, ax = plt.subplots(figsize=(10,6))
+lrtd = tddft.TDDFT(water_rks)
+lrtd.verbose = 4
+lrtd.nstates = 30
+lrtd.kernel()
+ax.stem(27.2116*lrtd.e, lrtd.oscillator_strength(), linefmt='grey', markerfmt=None, basefmt=" ", use_line_collection=True, label="LR-TDDFT")
 ax.plot(27.2116*mw, sigma, label="RT-TDDFT, strength=%e au"%field_strength)
 
 ax.legend(prop={'size': 10})
-ax.set_title("Adenine Gas-Phase 6-311+G(d)/TD-PBE0 Absorption", fontsize=20)
+ax.set_title('Water Gas-Phase 6-311+G(d)/TD-PBE0 Absorption', fontsize=20)
 ax.set_xlabel('Energy (eV)', fontsize=20)
 ax.set_ylabel('Absorption', fontsize=20)
 ax.set_xlim(0,40)
@@ -98,4 +74,3 @@ ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
 ax.grid(True)
 fig.tight_layout()
 fig.savefig("./test_spectrum_abos.pdf")
-
