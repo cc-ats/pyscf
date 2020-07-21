2b5c2d4fd5ec5367507fb10d841a223bed158375
yangjunjie <yangjunjie@MacBook-Pro.local>
2020-07-21 03:11:43 -0500

    test

--- pyscf/rt/test/test_resonant_water.py
+++ pyscf/rt/test/test_resonant_water.py
@@ -12,7 +12,7 @@ from   pyscf  import gto
 from   pyscf  import rt
 
 from pyscf.rt.util   import build_absorption_spectrum
-from pyscf.rt.field  import ClassicalElectricField, constant_field_vec, gaussian_field_vec
+from pyscf.rt.field  import ClassicalElectricField, constant_field_vec, sin_field_vec
 from pyscf.rt.result import read_index_list, read_step_dict, read_keyword_value
 
 
@@ -23,7 +23,7 @@ h2o =   gto.Mole( atom='''
   '''
   , basis='6-311+g(d)', symmetry=False).build()
 
-h2o_rks    = scf.RKS(h2o)
+h2o_rks          = scf.RKS(h2o)
 h2o_rks.verbose  = 0
 h2o_rks.xc       = "pbe0"
 h2o_rks.conv_tol = 1e-12
@@ -36,6 +36,22 @@ rttd.step_size      = 0.2
 rttd.verbose        = 4
 rttd.dm_ao_init     = dm_init
 rttd.prop_method    = "mmut"
+rttd.save_in_disk   = False
+rttd.chk_file       = None
+rttd.save_in_memory = True
+
+field_strength = 1e-4
+freq           = 0.28902542
+period         = 2*numpy.pi/freq
+sin_vec_z      = lambda t: [1e-4*sin(freq*t), 0.0, 0.0]
+sin_field_z    = ClassicalElectricField(h2o, field_func=sin_vec_z, stop_time=8.0*period)
+rttd.electric_field = sin_field_z
+rttd.kernel()
+
+time = read_keyword_value("t",      result_obj=rttd.result_obj)
+dip  = read_keyword_value("dipole", result_obj=rttd.result_obj)
+dxx  = dip[:,0] - dip[0,0]
+mw, sigma = build_absorption_spectrum(0.2, time[:7*rttd.total_step//8], dip[1*rttd.total_step//8+1:,:], damp_expo=50.0)
 
 lrtd = tddft.TDDFT(h2o_rks)
 lrtd.verbose = 4
@@ -47,47 +63,28 @@ lrtd.analyze()
 
 fig, (ax1,ax2) = plt.subplots(2,1,figsize=(10,10))
 ax1.stem(27.2116*lrtd.e, lrtd.oscillator_strength(), linefmt='grey', markerfmt=None, basefmt=" ", use_line_collection=True, label="LR-TDDFT, Oscillator Strength")
+ax1.plot(27.2116*mw, 2*sigma, label="RT-TDDFT, Freq=%4.4f au, Strength=%4.2e au"%(freq, field_strength))
 
-freq = 0.28902542
-period = 2*numpy.pi/freq
-gaussian_vec_z   = lambda t: [1e-4*sin(freq*t), 0.0, 0.0]
-gaussian_field_z = ClassicalElectricField(h2o, field_func=gaussian_vec_z, stop_time=8.0*period)
-
-rttd.save_in_disk   = True
-rttd.chk_file       = "h2o_z_%.2e.chk"%(1e-4)
-rttd.save_in_memory = False
-rttd.electric_field = gaussian_field_z
-rttd.kernel()
-
-time = read_keyword_value("t",      chk_file="h2o_z_%.2e.chk"%(1e-4))
-dip  = read_keyword_value("dipole", chk_file="h2o_z_%.2e.chk"%(1e-4))
-dxx  = dip[:,0] - dip[0,0]
-
-mw, sigma = build_absorption_spectrum(0.2, time[:7*rttd.total_step//8], dip[1*rttd.total_step//8+1:,:], damp_expo=50.0)
-ax1.plot(27.2116*mw, sigma, label="RT-TDDFT, Freq=%4.4f au, Strength=%4.2e au"%(0.372295453, 1e-4))
-
-ax1.legend()
-ax1.set_title("Water Gas-Phase 6-311+G(d)/TD-PBE0", fontsize=24)
+ax1.legend(prop={'size': 10})
+ax1.set_title(r'H$_2$O Gas-Phase 6-311+G(d)/TD-PBE0 Absorption', fontsize=16)
 ax1.set_xlabel('Energy (eV)', fontsize=16)
 ax1.set_ylabel('Absorption', fontsize=16)
-ax1.set_xlim(0,30)
+ax1.set_xlim(0,50)
 ax1.set_ylim(0,1)
 ax1.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
-ax1.grid(True)
 
 right_axis = ax2.twinx()
 left_axis = ax2
 p1, = left_axis.plot( time, dxx, label="Z-Dipole",color='C0' )
-p_, = left_axis.plot(-time, dxx, label="Field",color='C1' )
-p2, = right_axis.plot(time, [gaussian_field_z.get_field_vec(t)[0] for t in time], label="Field",color='C1')
+p_, = left_axis.plot(-time, dxx, label="Field",   color='C1' )
+p2, = right_axis.plot(time, [sin_field_z.get_field_vec(t)[0] for t in time], label="Field",color='C1')
  
-left_axis.set_xlabel('Time (fs)', fontsize=16)
+left_axis.set_xlabel('Time (fs)',   fontsize=16)
 left_axis.set_ylabel('Dipole (au)', fontsize=16)
 right_axis.set_ylabel('Field (au)', fontsize=16)
 right_axis.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
 left_axis.set_ylim(-0.016,0.016)
 left_axis.set_xlim(0,1600)
-left_axis.legend()
-ax2.grid(True)
+left_axis.legend(prop={'size': 10})
 fig.tight_layout()
 fig.savefig("./test_resonant_water.pdf")
--- pyscf/rt/test/test_spectrum.py
+++ pyscf/rt/test/test_spectrum.py
@@ -14,21 +14,21 @@ from pyscf.rt.util   import build_absorption_spectrum
 from pyscf.rt.field  import ClassicalElectricField, constant_field_vec, gaussian_field_vec
 from pyscf.rt.result import read_index_list, read_step_dict, read_keyword_value
 
-water =   gto.Mole( atom='''
+h2o =   gto.Mole( atom='''
   O     0.00000000    -0.00001441    -0.34824012
   H    -0.00000000     0.76001092    -0.93285191
   H     0.00000000    -0.75999650    -0.93290797
   '''
   , basis='6-311+g(d)', symmetry=False).build()
 
-water_rks    = scf.RKS(water)
-water_rks.verbose  = 0
-water_rks.xc       = "pbe0"
-water_rks.conv_tol = 1e-12
-water_rks.kernel()
-dm_init = water_rks.make_rdm1()
+h2o_rks    = scf.RKS(h2o)
+h2o_rks.verbose  = 0
+h2o_rks.xc       = "pbe0"
+h2o_rks.conv_tol = 1e-12
+h2o_rks.kernel()
+dm_init = h2o_rks.make_rdm1()
 
-rttd = rt.TDSCF(water_rks)
+rttd = rt.TDSCF(h2o_rks)
 rttd.total_step     = 8000
 rttd.step_size      = 0.2
 rttd.verbose        = 4
@@ -44,7 +44,7 @@ for i in range(3):
     temp_vec = [0.0, 0.0, 0.0]
     temp_vec[i] = field_strength
     temp_delta_vec   = lambda t: gaussian_field_vec(t, 0.0, 0.02, 0.0, temp_vec)
-    temp_delta_field = ClassicalElectricField(water, field_func=temp_delta_vec, stop_time=0.5)
+    temp_delta_field = ClassicalElectricField(h2o, field_func=temp_delta_vec, stop_time=0.5)
 
     rttd.electric_field = temp_delta_field
     rttd.kernel()
@@ -56,26 +56,24 @@ for i in range(3):
 
 mw, sigma = build_absorption_spectrum(0.2, time, numpy.array(dipole_list).T, damp_expo=50.0)
 
-fig, ax = plt.subplots(figsize=(10,6))
-lrtd = tddft.TDDFT(water_rks)
+lrtd = tddft.TDDFT(h2o_rks)
 lrtd.verbose = 4
 lrtd.nstates = 30
 lrtd.max_space = 100
 lrtd.max_space = 200
 lrtd.kernel()
 lrtd.analyze()
-print("lrtd.e = \n", lrtd.e)
-print("lrtd.converged = \n", lrtd.converged)
+
+fig, ax = plt.subplots(figsize=(10,6))
 ax.stem(27.2116*lrtd.e, lrtd.oscillator_strength(), linefmt='grey', markerfmt=None, basefmt=" ", use_line_collection=True, label="LR-TDDFT")
 ax.plot(27.2116*mw, 2*sigma, label="RT-TDDFT, strength=%4.2e au"%field_strength)
 
 ax.legend(prop={'size': 10})
-ax.set_title('Water Gas-Phase 6-311+G(d)/TD-PBE0 Absorption', fontsize=20)
-ax.set_xlabel('Energy (eV)', fontsize=20)
-ax.set_ylabel('Absorption', fontsize=20)
-ax.set_xlim(0,40)
+ax.set_title(r'H$_2$O Gas-Phase 6-311+G(d)/TD-PBE0 Absorption', fontsize=16)
+ax.set_xlabel('Energy (eV)', fontsize=16)
+ax.set_ylabel('Absorption', fontsize=16)
+ax.set_xlim(0,50)
 ax.set_ylim(0,1)
 ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
-ax.grid(True)
 fig.tight_layout()
 fig.savefig("./test_spectrum_abos.pdf")
